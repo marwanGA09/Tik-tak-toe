6245b01867c6abfd5094ca0da42b3a3563a84557
Adem Kedir <ademkedir724@gmail.com>
2024-02-02 14:06:12 +0300

    create gameControl IIFE
    - gameControl is used play game of in CLI
    - fix logical bug at playControl.checkStatus method

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const player = function (name, mark) {
-  this.mark = mark;
-  this.name = name;
+  let p_mark = mark;
+  let p_name = name;
 
   getMark = () => {
-    return this.mark;
+    return p_mark;
   };
 
   getName = () => {
-    return this.name;
+    return p_name;
   };
   return { getMark, getName };
 };
@@ -26,50 +26,103 @@ gameBoard = (function () {
 const playControl = (function () {
   let count = 0;
 
-  markAt = function (player, row, col) {
+  markAt = function (player, row, col, board) {
+    let gameBoard = board;
     if (!gameBoard[row][col]) {
       gameBoard[row][col] = player.getMark();
-      if (count < 6) {
+      if (count <= 6) {
         count++;
+        console.log(count);
       }
     }
+    return gameBoard;
   };
 
-  checkStatus = function () {
-    if (count >= 0) {
-      if (
-        ((gameBoard[0][0] === gameBoard[1][1]) === gameBoard[2][2]) !== "" ||
-        ((gameBoard[2][0] === gameBoard[1][1]) === gameBoard[0][2]) !== ""
-      ) {
-        return true;
-      } else {
-        for (let i = 0; i < 3; i++) {
-          let first = gameBoard[i][i];
-          if (
-            (first === gameBoard[i][(i + 1) % 3]) ===
-            gameBoard[i][(i + 2) % 3]
-          ) {
-            return true;
-          }
+  checkStatus = function (board) {
+    gameBoard = board;
+    if (count >= 5) {
+      for (let i = 0; i < 3; i++) {
+        if (
+          (gameBoard[i][i] === gameBoard[i][(i + 1) % 3] &&
+            gameBoard[i][i] === gameBoard[i][(i + 2) % 3] &&
+            gameBoard[i][i] !== "") ||
+          (gameBoard[i][i] === gameBoard[(i + 1) % 3][i] &&
+            gameBoard[i][i] === gameBoard[(i + 2) % 3][i] &&
+            gameBoard[i][i] !== "")
+        ) {
+          // console.log("End normal line");
+          return true;
         }
       }
+
+      if (
+        (gameBoard[1][1] !== "" &&
+          gameBoard[2][0] === gameBoard[1][1] &&
+          gameBoard[1][1] === gameBoard[0][2]) ||
+        (gameBoard[1][1] !== "" &&
+          gameBoard[0][0] === gameBoard[1][1] &&
+          gameBoard[1][1] === gameBoard[2][2])
+      ) {
+        // console.log(gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]);
+        return true;
+      }
     }
-    console.log("false");
     return false;
   };
 
   return { markAt, checkStatus };
 })();
 
-// const player1 = player("adem", "x");
-// console.log(player1.getName());
-// console.log(player1.getMark());
-// const player2 = player("kedir", "0");
-// console.log(player2.getName());
-// console.log(player2.getMark());
+function display(board) {
+  for (i = 0; i < board.length; i++) {
+    console.log(board[i]);
+  }
+}
 
-// playControl.markAt(player1, 0, 2);
-// playControl.markAt(player1, 1, 1);
-// playControl.markAt(player1, 2, 0);
-// console.log("jksdfjs", playControl.checkStatus());
-// console.log();
+// display(gameBoard);
+const gameControl = function () {
+  const playerName1 = prompt("name for player 1?");
+  const playerName2 = prompt("name for player 2?");
+  const player1 = player(playerName1, "X");
+  const player2 = player(playerName2, "O");
+  console.log(`player 1 ${player1.getName()}, with mark ${player1.getMark()}`);
+  console.log(`player 2 ${player2.getName()}, with mark ${player2.getMark()}`);
+  let end = false;
+  let col, row;
+  let currentPlayer = player1;
+  let mainBoard = gameBoard;
+  console.log("basic main board");
+  display(mainBoard);
+  while (!end) {
+    console.log("current player is ", currentPlayer.getName());
+    row = prompt(`${currentPlayer.getName()}, row? `);
+    col = prompt(`${currentPlayer.getName()}, col? `);
+    mainBoard = playControl.markAt(currentPlayer, row, col, mainBoard);
+    if (playControl.checkStatus(mainBoard)) {
+      end = true;
+      console.log("game end", currentPlayer.getName(), "win");
+    } else {
+      console.log("game does not end");
+      currentPlayer =
+        currentPlayer.getName() === player1.getName() ? player2 : player1;
+    }
+    display(mainBoard);
+  }
+
+  console.log("finish");
+};
+// console.log("a" === "a" && "a" === "a");
+gameControl();
+// const playerName1 = prompt("name for player 1?");
+// const playerName2 = prompt("name for player 2?");
+// const player1 = player(playerName1, "X");
+// const player2 = player(playerName2, "O");
+// let player1 = player("adem", "x");
+// let player2 = player("kedir", "0");
+
+// console.log(player1.getName());
+// // console.log(player2);
+// console.log(`player 1 ${player1.getName()}, with mark ${player1.getMark()}`);
+// console.log(`player 2 ${player2.getName()}, with mark ${player2.getMark()}`);
+
+// console.log(gameBoard);
